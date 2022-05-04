7dd86c3ac5f54a3621d6a9721be54305b618ee89
Fotsing Didier <70779391+F-qu@users.noreply.github.com>
2022-05-04 02:05:47 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>404 NOT FOUND</title>
+</head>
+<body>
+    <section class="flex">
+
+    <img src="images/Scarecrow.png" alt="Scarecrow.png">
+
+    <div class="content">
+        <h2>I have bad news for you</h2>
+        <p>The page you are looking for might removed or is temporaly unavailable </p>
+        <a href="#">back to homepage</a>
+    </div>
+
+    </section>
+
+    <p class="footer">
+        Created by <strong>@didier_snake</strong> - devChallenges.io
+    </p>
+    
+</body>
+</html>
